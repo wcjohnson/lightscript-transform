fc6096cc18dcad1a92877f0391d8eb9f0f7ca715
William Johnson <wcjohnson@oigroup.net>
2018-09-18 20:23:57 -0700

    test: Formatting, disable irrelevant whitespace tests

--- test/fixtures/whitespace/block-termination/output.js
+++ test/fixtures/whitespace/block-termination/output.js
@@ -1,17 +1,18 @@
 import result from "lodash/result";
-({ entry: function func() {
+({
+  entry: function func() {
     return result;
-  }, entry2: something,
+  },
+  entry2: something,
   entry3: function reactFunc() {
     return <SomeJSX>
     </SomeJSX>;
-  }, entry4: somethingElse
+  },
+  entry4: somethingElse
 });
-
 [function func() {
   return result;
 }, something];
-
 call(function func() {
   return result;
 }, arg2);
@@ -20,12 +21,17 @@ do a; while (b);
 
 function f() {
   a;
+
   while (b) {
     c;
   }
-}function g() {
+}
+
+function g() {
   do a; while (b);
+
   c;
+
   while (d) {
     e;
   }
